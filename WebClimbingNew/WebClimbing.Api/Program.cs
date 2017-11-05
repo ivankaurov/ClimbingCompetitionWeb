e87d5a672aaa5f1a0312913d712d6146686de2d2
katerina92 <katerina010192@mail.ru>
2017-11-05 14:49:43 +0300

    API project

---
+++ WebClimbingNew/WebClimbing.Api/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Climbing.Web.Database;
+
+namespace Climbing.Web.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = BuildWebHost(args);
+            Console.WriteLine("Migrating...");
+            using(var scope = host.Services.CreateScope())
+            using (var context = scope.ServiceProvider.GetRequiredService<ClimbingContext>())
+            {
+                context.Database.Migrate();
+                Console.WriteLine("Migration completed");
+            }
+            host.Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
